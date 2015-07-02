1968fc6ad5fe55c3ec539bb5878c8a41e28507aa
Jens Hauke <jens@4k2.de>
2015-07-02 16:00:02 +0200

    ili9183: Add README with connection schema.

--- app/ili9163/hello_world.lua
+++ app/ili9163/hello_world.lua
@@ -1,43 +1,51 @@
 
-print (ili9163.hw)
+count = 0
 
 function prepare()
-     disp:setFont(u8g.font_6x10)
-     disp:setFontRefHeightExtendedText()
-     disp:setDefaultForegroundColor()
-     disp:setFontPosTop()
+	disp:setFont(u8g.font_6x10)
+	disp:setFontRefHeightExtendedText()
+	disp:setDefaultForegroundColor()
+	disp:setFontPosTop()
 end
 
-function start()
-    local x
-    disp = u8g.ili9163c(sla)
-
-    prepare()
-
-    disp:setRot90()
-    disp:firstPage()
-    repeat
+function draw()
 	for x = 10, 120, 1 do
-	    disp:setRGB(255-2*x, x, 2*x)
-	    disp:drawPixel(x, 60)
-	    disp:drawPixel(x, 61)
-	    disp:drawPixel(x, 62)
+		disp:setRGB(255-2*x, x, 2*x)
+		for y = 60, 70 do
+			disp:drawPixel(x, y)
+		end
+		tmr.wdclr()
 	end
 	disp:setRGB(255, 255, 0)
+	disp:setFont(u8g.font_6x10)
 	disp:drawStr(30, 30, "Hello World!")
-
+	
+	-- disp:drawStr(10, 100, ""..count)
+	
 	disp:setRGB(255, 128, 0)
 	disp:setFont(u8g.font_chikita)
-	disp:drawStr(60, 80, "- Jens")
-	
-    until disp:nextPage() == false
-
-    print ("Ende")
+	disp:drawStr(90, 120, "- Jens")
 end
 
-start()
 
+function start()
+	local x
+	if (not disp) then
+		disp = u8g.ili9163c(sla)
+		prepare()
+	end
+
+	disp:setRot90()
+	disp:firstPage()
+	repeat
+		draw()
+	until disp:nextPage() == false
+	count = count + 1
+end
+
+tmr.alarm(0, 5000, 1, start)
 
 -- Local Variables:
 --  compile-command: "nodemcu-uploader.py exec hello_world.lua"
 -- End:
+--  compile-command: "nodemcu-uploader.py upload hello_world.lua:init.lua"
