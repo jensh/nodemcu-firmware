78641da4073538918671d7fbe7f6745219a18501
Jens Hauke <jens@4k2.de>
2015-07-02 18:04:57 +0200

    ili9183: Colorful "Hello World".

--- app/ili9163/hello_world.lua
+++ app/ili9163/hello_world.lua
@@ -8,17 +8,43 @@ function prepare()
 	disp:setFontPosTop()
 end
 
+function ccyle(p)
+	local x,a,b
+	p=p % 1536
+	x=p % 512 a=x b=255
+	if (x >= 256) then
+		a=255
+		b=511-x
+	end
+	if (p < 512) then
+		disp:setRGB(b,a,0)
+	elseif (p < 1024) then
+		disp:setRGB(0,b,a)
+	else
+		disp:setRGB(a,0,b)
+	end
+end
+
 function draw()
-	for x = 10, 120, 1 do
-		disp:setRGB(255-2*x, x, 2*x)
-		for y = 60, 70 do
+	for x = 0, 128 do
+		ccyle(x * 22)
+		for y = 35, 45 do
 			disp:drawPixel(x, y)
 		end
 		tmr.wdclr()
 	end
-	disp:setRGB(255, 255, 0)
+	disp:setRGB(255, 255, 255)
 	disp:setFont(u8g.font_6x10)
-	disp:drawStr(30, 30, "Hello World!")
+	disp:drawStr(30, 15, "Hello World!")
+
+	disp:drawStr(5, 60, "ESP8266-01 with")
+	disp:drawStr(5, 70, "128*128px SPI")
+	disp:drawStr(5, 80, "display (ILI9163c)")
+
+	disp:setFont(u8g.font_chikita)
+	disp:drawStr(0, 100, "https://github.com/jensh/")
+	disp:drawStr(5, 107, "nodemcu-firmware/")
+	disp:drawStr(5, 114, "tree/jh/ili9163c")
 	
 	-- disp:drawStr(10, 100, ""..count)
 	
@@ -43,7 +69,8 @@ function start()
 	count = count + 1
 end
 
-tmr.alarm(0, 5000, 1, start)
+--start()
+tmr.alarm(0, 5000, 0, start)
 
 -- Local Variables:
 --  compile-command: "nodemcu-uploader.py exec hello_world.lua"
